e99545897e4273a717976a4d50c5fafe1886c039
@yuanerlon <elyuan@kaikeba.com>
2020-07-31 16:59:16 +0800

    xx

--- cloud-mq/src/main/java/xxh/receiver/DeadMqReceiver.java
+++ cloud-mq/src/main/java/xxh/receiver/DeadMqReceiver.java
@@ -28,14 +28,14 @@ public class DeadMqReceiver {
         Exception exception = null;
         try {
             if (msg.contains("deadletter")){
-                throw new RuntimeException("dead letter exception");
+                ack = false;
+                //throw new RuntimeException("dead letter exception");
             }
         } catch (Exception e){
             ack = false;
-            exception = e;
         }
         if (!ack){
-            log.error("消息消费发生异常，error msg:{}", exception.getMessage(), exception);
+            //log.error("消息消费发生异常，error msg:{}", exception.getMessage(), exception);
             channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
         } else {
             channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
